3a9b399655b1b84755de04536200e1cdb5725989
ChrisGemps <chrisalin.gemparo@cit.edu>
2025-10-22 11:28:14 +0800

    Setup: Springboot

---
+++ lazzappe/lazzappe/src/main/java/com/lazzappe/lazzappe/LazzappeApplication.java
@@ -0,0 +1,13 @@
+package com.lazzappe.lazzappe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazzappeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazzappeApplication.class, args);
+	}
+
+}
